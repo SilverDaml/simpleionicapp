d8892ec4136a11bf69f99789a5ff45b472977b46
Francisco Arellano <SilverD.amld@gmail.com>
2023-04-14 12:19:37 +0000

    Initial commit

--- simpleionicapp/src/app/home/home.page.ts
+++ simpleionicapp/src/app/home/home.page.ts
@@ -10,11 +10,15 @@ export class HomePage {
   constructor() {}
     myVariable: string ="The force is with me!";
     myVariable2: string ="Hello from CCT";
+    myVariable3: string ="Third button";
     
     updateMyValue(){
       this.myVariable = "Now the force is even stronger!!";
     }
     updateMyValue2(){
       this.myVariable2 = "Hello from Ionic Mobile Development";
+    }
+    updateMyValue3(){
+      this.myVariable3 = "Three!!";
     }
 }
